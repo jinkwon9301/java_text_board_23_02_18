66a0da7f2df484555ea61596842a1716f2ce850c
jinkwon9301 <valor9254@gmail.com>
2023-02-18 19:17:05 +0900

    입력받은 게시물 정보를 Article 객체에 저장 후 객체 변수 출력

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -20,7 +20,13 @@ public class Main {
         System.out.printf("내용 : ");
         String body = sc.nextLine();
         int id = ++articleLastId;
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+        Article article = new Article();
+        article.id = id;
+        article.title = title;
+        article.body = body;
+
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       } else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
       }
@@ -30,4 +36,10 @@ public class Main {
 
     sc.close();
   }
+
+}
+class Article {
+  int id;
+  String title;
+  String body;
 }
