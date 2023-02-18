1d698bed3a9456415bc62a1e83a545a48168754c
jinkwon9301 <valor9254@gmail.com>
2023-02-18 17:58:29 +0900

    명령어 입력받기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,7 +1,18 @@
 package com.sbs.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.printf("명령 ) ");
+    String result = sc.nextLine();
+    System.out.printf("입력된 명령어 : %s\n", result);
+    System.out.println("== 프로그램 종료 ==");
+
+    sc.close();
   }
 }
