0792dcbae5c6064688c424a030c92538b99485fd
jinkwon9301 <valor9254@gmail.com>
2023-02-18 22:49:38 +0900

    usr/article/detail 명령어로 최근 게시물 보기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -2,10 +2,16 @@ package com.sbs.exam.board;
 
 import java.util.Scanner;
 
+/*
+[] usr/article/detail 입력처리
+[] usr/article/detail 입력되면 가장 최근 게시물 정보 노출
+*/
+
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
+    Article lastArticle = null;
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
@@ -13,6 +19,18 @@ public class Main {
       System.out.printf("명령 ) ");
       String cmd = sc.nextLine();
       if (cmd.equals("exit")) break;
+      else if (cmd.equals("usr/article/detail")) {
+
+        if (lastArticle == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+        Article article = lastArticle;
+        System.out.println("== 게시물 상세내용 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.body);
+      }
       else if (cmd.equals("usr/article/write")) {
         System.out.println("== 게시물 등록 ==");
         System.out.printf("제목 : ");
@@ -21,6 +39,7 @@ public class Main {
         String body = sc.nextLine();
         int id = ++articleLastId;
         Article article = new Article(id, title, body);
+        lastArticle = article;
 
         System.out.println("생성된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
