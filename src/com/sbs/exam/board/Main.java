18186cd17815a80601d15ed911e7cba9fb286ccf
jinkwon9301 <valor9254@gmail.com>
2023-02-18 18:15:00 +0900

    exit 명령이 나올 때까지 계속 입력받기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -8,9 +8,13 @@ public class Main {
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.printf("명령 ) ");
-    String result = sc.nextLine();
-    System.out.printf("입력된 명령어 : %s\n", result);
+    while (true) {
+      System.out.printf("명령 ) ");
+      String result = sc.nextLine();
+      if (result.equals("exit")) break;
+      System.out.printf("입력된 명령어 : %s\n", result);
+    }
+
     System.out.println("== 프로그램 종료 ==");
 
     sc.close();
