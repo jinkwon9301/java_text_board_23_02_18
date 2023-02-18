dd4a8e2b124fd4c4259393496ae3dd920c26a348
jinkwon9301 <valor9254@gmail.com>
2023-02-18 22:05:30 +0900

    매개변수있는 생성자를 추가하여, 게시물 객체를 쉽게 만들도록

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -20,10 +20,7 @@ public class Main {
         System.out.printf("내용 : ");
         String body = sc.nextLine();
         int id = ++articleLastId;
-        Article article = new Article();
-        article.id = id;
-        article.title = title;
-        article.body = body;
+        Article article = new Article(id, title, body);
 
         System.out.println("생성된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
@@ -42,6 +39,12 @@ class Article {
   int id;
   String title;
   String body;
+
+  Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
 
   @Override
   public String toString() {
