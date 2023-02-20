9f3b731160249dd9bf715cf8cf71c06cbe0f0603
jinkwon9301 <valor9254@gmail.com>
2023-02-20 17:42:30 +0900

    입력된 id에 대해서 유효성 체크

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -41,8 +41,18 @@ public class Main {
         }
       }
       else if (rq.getUrlPath().equals("usr/article/detail")) {
+        int id = 0;
 
-        int id = Integer.parseInt(params.get("id"));
+        try {
+          id = Integer.parseInt(params.get("id"));
+        } catch (NumberFormatException e) {
+          System.out.println("id를 정수 형태로 입력해주세요.");
+          continue;
+        }
+
+        if (params.containsKey("id") == false) {
+          System.out.println("id를 입력해주세요.");
+        }
 
         Article article = articles.get(id - 1);
 
