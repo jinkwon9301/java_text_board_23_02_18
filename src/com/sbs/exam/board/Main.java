cbb75874fc1bda9cad8e18d75c7a1b849a810f84
jinkwon9301 <valor9254@gmail.com>
2023-02-20 18:43:42 +0900

    Util.reverseList를 이용해서 정렬로직 간소화

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -38,19 +38,18 @@ public class Main {
 
         boolean orderByDesc = true;
 
+        List<Article> sortedArticles = articles;
+
         if (params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
           orderByDesc = false;
         }
 
-        if (orderByDesc ) {
-          for (int i = articles.size() - 1; i >= 0; i--) {
-            Article article = articles.get(i);
+        if (orderByDesc) {
+          sortedArticles = Util.reverseList(sortedArticles);
+        }
+
+        for (Article article : sortedArticles) {
             System.out.printf("%d / %s\n", article.id, article.title);
-          }
-        } else {
-          for (Article article : articles) {
-            System.out.printf("%d / %s\n", article.id, article.title);
-          }
         }
 
         System.out.println("-------------------");
@@ -164,5 +163,15 @@ class Util {
   static String getUrlPathFromUrl(String url) {
 
     return url.split("\\?", 2)[0];
+  }
+
+  // 이 함수는 원본리스트를 훼손하지 않고, 새 리스트를 만듭니다. 즉 정렬이 반대인 복사본리스트를 만들어서 반환합니다.
+  public static<T> List<T> reverseList(List<T> list) {
+    List<T> reverse = new ArrayList<>(list.size());
+
+    for ( int i = list.size() - 1; i >= 0; i-- ) {
+      reverse.add(list.get(i));
+    }
+    return reverse;
   }
 }
