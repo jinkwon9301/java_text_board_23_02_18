c5d7c7ba0bd9d7cf9aeaccbd3a1fc5a77fee42fa
jinkwon9301 <valor9254@gmail.com>
2023-03-01 09:23:08 +0900

    롬복 적용

--- src/main/java/com/sbs/exam/board/Article.java
+++ src/main/java/com/sbs/exam/board/Article.java
@@ -1,15 +1,16 @@
 package com.sbs.exam.board;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Article {
   int id;
   String title;
   String body;
-
-  Article(int id, String title, String body) {
-    this.id = id;
-    this.title = title;
-    this.body = body;
-  }
 
   @Override
   public String toString() {
